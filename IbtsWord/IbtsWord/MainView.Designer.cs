991130e234c4f74e664b4ccab0d16bbf66895ebe
JasonDevStudio <ylsoo@ylsoo.net>
2014-08-11 17:52:28 +0800

    流程梳理

--- IbtsWord/IbtsWord/MainView.Designer.cs
+++ IbtsWord/IbtsWord/MainView.Designer.cs
@@ -36,6 +36,8 @@
             this.operateDate = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.stageName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.stateName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dvTaskList)).BeginInit();
             this.SuspendLayout();
             // 
@@ -100,11 +102,35 @@
             this.stateName.HeaderText = "State";
             this.stateName.Name = "stateName";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(479, 6);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Read Word";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(355, 6);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Read Word";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // MainView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(741, 310);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.dvTaskList);
             this.Controls.Add(this.btn_getTaskList);
             this.Margin = new System.Windows.Forms.Padding(2);
@@ -128,5 +154,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn operateDate;
         private System.Windows.Forms.DataGridViewTextBoxColumn stageName;
         private System.Windows.Forms.DataGridViewTextBoxColumn stateName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- IbtsWord/IbtsWord/MainView.cs
+++ IbtsWord/IbtsWord/MainView.cs
@@ -9,11 +9,12 @@ using System.Windows.Forms;
 using System.Collections;
 using System.IO;
 using System.Runtime.Serialization.Json;
+using Word = Microsoft.Office.Interop.Word;
 
 namespace IbtsWord
 {
     public partial class MainView : Form
-    { 
+    {
 
         public MainView()
         {
@@ -27,14 +28,14 @@ namespace IbtsWord
 
             StreamReader strRead = new StreamReader(AppDomain.CurrentDomain.BaseDirectory + "json.txt");
             result = strRead.ReadToEnd();
-                
+
             //pm.getTaskList();
 
-            System.Data.DataTable table = new DataTable("ParentTable");
+            System.Data.DataTable table = new System.Data.DataTable("ParentTable");
             // Declare variables for DataColumn and DataRow objects.
             DataColumn column;
-            DataRow row;           
-             
+            DataRow row;
+
 
             table.Columns.AddRange(new DataColumn[] { 
                                          new DataColumn("taskId", typeof(string)),
@@ -68,7 +69,7 @@ namespace IbtsWord
 
             //取消dataGridView1按默认方式显示
             dvTaskList.AutoGenerateColumns = false;
-            
+
             dvTaskList.DataSource = table;
 
             DataGridViewTextBoxColumn column1 = new DataGridViewTextBoxColumn();
@@ -108,17 +109,99 @@ namespace IbtsWord
 
         private void dvTaskList_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-            DataGridView dgv = (DataGridView)sender;//这行语句也可以不要，如果已经创建了dgv，详见航道系统的代码。 
-            //如果是"Button"列，按钮被点击 
-            if (dgv.Columns[e.ColumnIndex].Name == "Button")//此处索引列可以使name、也可以使headertext，看具体的设置。      
-            { 
-                MessageBox.Show(e.RowIndex.ToString() +              "行的按钮被点击了。");      
+            DataGridView dgv = (DataGridView)sender;//这行语句也可以不要，如果已经创建了dgv，详见航道系统的代码。 
+            //如果是"Button"列，按钮被点击 
+            if (dgv.Columns[e.ColumnIndex].Name == "Button")//此处索引列可以使name、也可以使headertext，看具体的设置。      
+            {
+                MessageBox.Show(e.RowIndex.ToString() + "行的按钮被点击了。");
             }
         }
 
         private void MainView_FormClosed(object sender, FormClosedEventArgs e)
         {
-            Application.Exit();
+            System.Windows.Forms.Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+            
+            
+
+
+            Word.Application app = new Word.Application() ;   //定义应用程序对象 
+            Word.Document doc = null;   //定义 word 文档对象
+            Object saveChanges = app.Options.BackgroundSave;//文档另存为 
+            Object missing = System.Reflection.Missing.Value; //定义空变量
+
+            try
+            {
+                
+                Object isReadOnly = false;
+                object filePath = AppDomain.CurrentDomain.BaseDirectory + "test.dotx";//文档路径 
+                doc = app.Documents.Open(ref filePath, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing);
+                 
+
+                doc.Activate();//激活文档
+                object bookmarkName = "richTextContentControl1";
+                foreach (Word.ContentControl item in app.ActiveDocument.ContentControls)
+                {
+                    if (item.Tag =="Information")
+                    {
+                        item.SetPlaceholderText(Text: "紫川软件..."); 
+                    }
+                }
+
+                object savePath = AppDomain.CurrentDomain.BaseDirectory + "test.docx";   //文档另存为的路径 
+                
+                doc.SaveAs(ref savePath, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing, ref missing);
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                doc.Close(ref saveChanges, ref missing, ref missing);//关闭文档
+                app.Quit(ref missing, ref missing, ref missing);//关闭应用程序
+            }
+           
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var filePath = AppDomain.CurrentDomain.BaseDirectory + "test.dotx";//文档路径 
+            Word.Application appWord = new Word.Application();
+            Word.Document doc = new Word.Document();
+            object oMissing = System.Reflection.Missing.Value;
+            //打开模板文档，并指定doc的文档类型
+            object objTemplate = filePath;
+            object objDocType = Word.WdDocumentType.wdTypeDocument;
+            object objFalse = false;
+            object objTrue = true;
+            doc = (Word.Document)appWord.Documents.Add(ref objTemplate, ref objFalse, ref objDocType, ref objTrue);
+
+            foreach (Word.ContentControl item in appWord.ActiveDocument.ContentControls)
+            {
+                if (item.Tag == "Information")
+                {
+                    item.Range.Text = "紫川软件测试";
+                    
+                }
+            }
+             
+
+            object savePath = AppDomain.CurrentDomain.BaseDirectory + "test1.docx";   //文档另存为的路径 
+            object missingValue = Type.Missing;
+            object miss = System.Reflection.Missing.Value; 
+            object doNotSaveChanges = Word.WdSaveOptions.wdDoNotSaveChanges;
+            doc.SaveAs2(ref savePath);
+            doc.Close(ref doNotSaveChanges, ref missingValue, ref missingValue);
+            appWord.Application.Quit(ref miss, ref miss, ref miss);
+            doc = null;
+            appWord = null; 
         }
     }
 }
